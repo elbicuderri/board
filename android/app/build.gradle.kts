22c4fb0e38b450f775074e8b6718f15f55cceba1
elbicuderri <muger1031@gmail.com>
2025-05-12 19:19:07 +0900

    Modify android files

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,12 +7,13 @@ plugins {
 
 android {
     namespace = "com.example.writing"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 34 // Explicitly set compileSdk to 34 (Android 14)
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -24,10 +25,11 @@ android {
         applicationId = "com.example.writing"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 21 // Set minSdk to 21 (Android 5.0) for broader compatibility
+        targetSdk = 34 // Set targetSdk to 34 (Android 14)
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -35,8 +37,17 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+        debug {
+            isMinifyEnabled = false
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
 
 flutter {
